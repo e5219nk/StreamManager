5798fb8407efeb36dc955a9f820645e5197db91d
e5219nk <endtheboss@gmail.com>
2025-04-10 16:51:58 +0900

    型定義を追加してanyを排除

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,6 @@
 module.exports = {
     extends: ['next', 'next/core-web-vitals'],
     rules: {
-      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-explicit-any': 'warn',
     },
   }
